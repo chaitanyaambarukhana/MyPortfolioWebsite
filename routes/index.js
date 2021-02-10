c1c79bc1b96debcaeee57088f8d679952b4c4358
Chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2021-02-10 21:02:44 +0530

    added server.js and updated package.json to support for heroku

--- routes/index.js
+++ routes/index.js
@@ -6,35 +6,36 @@ let router = express.Router();
 router.use(express.urlencoded());
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' });//route to "/"
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' });//route to "/home"
 });
 
 
 //about page
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About me' });
+  res.render('about', { title: 'About me' });//route to about page
 });
 
 //projects page
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });//route to projects page
 });
 
 //services page
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+  res.render('services', { title: 'Services' });//route to services page
 });
 
 //contact page
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });//route to contact page
 });
 
-router.post('/contact',function(req,res){
+router.post('/contact',function(req,res)//post method requested is prcoessed here, redirecting to home page and collecting the name, email and message
+{
  //console.log(req.body.name);
  console.log(req.body.name);
  console.log(req.body.email);
